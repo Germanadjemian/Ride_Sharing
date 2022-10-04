6e3c3fbefc04ad24d6d2279eb7400f6bc942d975
German Adjemian <GERMAN.ADJEMIAN@correo.ucu.edu.uy>
2022-10-04 18:59:43 -0300

    ahora si

---
+++ src/Library/Funciones.cs
@@ -0,0 +1,13 @@
+﻿namespace Library;
+public class Funciones //aca pongan las clases metodos y demas
+{
+    public static int funcion1()
+    {
+        return 1;
+    }
+    public static int funcion2()
+    {
+        return 2;
+    }
+}
+
---
+++ src/Program/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Library;
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int Sumando= Funciones.funcion1()+Funciones.funcion2();
+        Console.Write(Sumando);
+
+
+    }
+}

ef4aa45b62bfad730f2e8139dd94cffb7756076b
sebamart12 <93790958+sebamart12@users.noreply.github.com>
2022-10-06 16:21:40 -0300

    Pool and Normal

---
+++ src/Library/Normal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    //La clase normal representa un tipo de driver 
+    //que solo es capaz de llevar  a una persona.
+    public class Normal : Driver
+    {
+        
+    }
+
+}
---
+++ src/Library/Pool.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    //La clase Pool representa un tipo de driver 
+    //que puede llevar mas de una persona, respentando la capacidad máxima del vehículo.
+    public class Pool : Driver
+    {
+        
+    }
+}
